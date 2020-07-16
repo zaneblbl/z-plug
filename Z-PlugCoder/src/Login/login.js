89085b2bf4ec530d403a34a5e48b11f1203ec592
zz <549024307@qq.com>
2020-07-16 16:55:15 +0800

    no message

--- Z-PlugCoder/src/Login/login.js
+++ Z-PlugCoder/src/Login/login.js
@@ -1,60 +1,66 @@
 require('./login.css')
-// import common from '../../Common/common'
+import common from '../../Common/common'
 let login = {
+  clientID: 'c92b731ae212fbc66088',
+  clientSecret: 'd3f4065441e8ed9d2d6a965af327348c570523ea',
   init() {
-    // let loginBtn = document.createElement('a')
-    // loginBtn.id = 'zPlug__login__btn'
-    // loginBtn.classList.add('zPlug__login__btn')
-    // loginBtn.innerHTML = `<div>Login</div>`
-    // loginBtn.href = `https://zaneblbl.github.io/z-plug/`
-    // window.document.body.appendChild(loginBtn);
-    this.loginListener()
-  },
-  toLogin(){
-      console.log('---------');
+    if (window.location.href.indexOf(`https://zaneblbl.github.io/z-plug`) != -1) {
+      this.loginListener()
+    } else {
+      let loginBtn = document.createElement('a')
+      loginBtn.id = 'zPlug__login__btn'
+      loginBtn.classList.add('zPlug__login__btn')
+      loginBtn.innerHTML = `<div>Login</div>`
+      loginBtn.href = `https://zaneblbl.github.io/z-plug/`
+      window.document.body.appendChild(loginBtn);
+    }
 
-      chrome.extension.sendMessage({
-        'url': `https://github.com/login/oauth/access_token?client_id=${e.data.clientID}&client_secret=${e.data.clientSecret}&code=${e.data.code}`,
-        'type': 'get'
-      }, function (res) {
-        console.log(res.data.access_token);
-        if (chrome.storage) {
-          chrome.storage.sync.set({
-            access_token: res.data.access_token,
-          }, function () {
-            document.write('login success')
-          });
-        }
-      });
-    
+  },
+  toLogin(param) {
+    let self = this
+    chrome.extension.sendMessage({
+      'url': `https://github.com/login/oauth/access_token?client_id=${param.clientID}&client_secret=${param.clientSecret}&code=${param.code}`,
+      'type': 'get'
+    }, function (res) {
+      let token = self.getToken(res, 'access_token')
+      if (chrome.storage) {
+        chrome.storage.sync.set({
+          access_token: token,
+        }, function () {
+          document.write('login success')
+        });
+      }
+    });
+
   },
   loginListener() {
     let xhr = new XMLHttpRequest()
-    let clientID = `c92b731ae212fbc66088`
-    let clientSecret = `d3f4065441e8ed9d2d6a965af327348c570523ea`
+    let clientID = this.clientID
+    let clientSecret = this.clientSecret
     let url = `https://github.com/login/oauth/authorize?client_id=${clientID}`
-
-    let code = this.getQueryVariable('code')
+    let code = common.getQueryVariable('code')
     if (code) {
-      console.log('22222222');
-      
-      let param={'clientID': clientID,'clientSecret':clientSecret,'code':code}
-
+      let param = {
+        'clientID': clientID,
+        'clientSecret': clientSecret,
+        'code': code
+      }
+      this.toLogin(param)
     } else {
       window.location.href = url
-    } 
+    }
   },
-  getQueryVariable(variable) {
-    var query = window.location.search.substring(1)
+  getToken(string, variable) {
+    var query = string
     var vars = query.split('&')
     for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split('=')
-        if (pair[0] == variable) {
-            return pair[1]
-        }
+      var pair = vars[i].split('=')
+      if (pair[0] == variable) {
+        return pair[1]
+      }
     }
     return (false)
-}
+  },
 }
 
 login.init()
