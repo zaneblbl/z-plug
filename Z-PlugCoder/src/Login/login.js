11776a9a18cb39f32e3c817c31bc5073a0bf1c17
zz <549024307@qq.com>
2020-07-16 14:37:07 +0800

    no message

--- Z-PlugCoder/src/Login/login.js
+++ Z-PlugCoder/src/Login/login.js
@@ -1,20 +1,28 @@
 require('./login.css')
 // import common from '../../Common/common'
-let login={
-  init(){
-    let loginBtn=document.createElement('a')
-    loginBtn.id='zPlug__login__btn'
+let login = {
+  init() {
+    let loginBtn = document.createElement('a')
+    loginBtn.id = 'zPlug__login__btn'
     loginBtn.classList.add('zPlug__login__btn')
-    loginBtn.innerHTML=`<div>Login</div>`
-    loginBtn.href=`https://zaneblbl.github.io/z-plug/`
+    loginBtn.innerHTML = `<div>Login</div>`
+    loginBtn.href = `https://zaneblbl.github.io/z-plug/`
     window.document.body.appendChild(loginBtn);
 
     this.loginListener()
   },
-  loginListener(){
-    window.addEventListener("message", function(e)
-    {
-      console.log(e.data);
+  loginListener() {
+    window.addEventListener("message", function (e) {
+      background_ajax('get', `https://github.com/login/oauth/access_token?client_id=${e.data.clientID}&client_secret=${e.data.clientSecret}&code=${e.data.code}`).then(res => {
+        console.log(res.data.access_token);
+        if (chrome.storage) {
+          chrome.storage.sync.set({
+            access_token: res.data.access_token,
+          }, function () {
+            document.write('login success')
+          });
+        }
+      })
     }, false);
   }
 }
