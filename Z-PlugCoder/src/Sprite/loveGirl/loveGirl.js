97e9639ac2e60989e98fb7cd87a19f3fc677fc02
zz <549024307@qq.com>
2020-07-29 14:32:55 +0800

    no message

--- Z-PlugCoder/src/Sprite/loveGirl/loveGirl.js
+++ Z-PlugCoder/src/Sprite/loveGirl/loveGirl.js
@@ -8,41 +8,29 @@ let loveGirl = {
   createBonesDom(bones) {
     // 使用虚拟dom创建节点
     let createEl = (tagName, props, children) => new CreactEl(tagName, props, children)
-    // const vdom = createEl('div', {
-    //   'id': 'box'
-    // }, [
-    //   createEl('h1', {
-    //     style: 'color: pink'
-    //   }, ['I am H1']),
-    //   createEl('ul', {
-    //     class: 'list'
-    //   }, [createEl('li', ['#list1']), createEl('li', ['#list2'])]),
-    //   createEl('p', ['I am p'])
-    // ])
     let vdom=``
     let result=this.childrenDom(bones,bones.filter(res=>res.name=='root')[0])
     vdom=createEl('div',{'id':'root'},result)
     let rootnode = vdom.render()
     document.body.appendChild(rootnode)
   },
+  // 父子分组
   childrenDom(bones,root){
     let children=[]
     bones.forEach(bone => {
       if(bone.parent==root.name){
-        bone.children=this.childrenDom(bones,bone)
-        // new CreactEl('div', {'id':bone.name}, children)
-        children.push(bone)
+        let creatEl=new CreactEl('div',{id:bone.name},this.childrenDom(bones,bone))
+        children.push(creatEl)
       }
     });
-
     return children
   },
+  // 初始化json
   initJSON() {
     this.boneJson = getJson
     console.log(this.boneJson);
     let bones = this.boneJson.bones
     this.createBonesDom(bones)
-
   }
 }
 
